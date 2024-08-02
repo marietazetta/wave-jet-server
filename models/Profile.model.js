a14e97d697bc72f4d9bf097582717a8ab7a03bc3
Diego Cerezo <diego.cerezo@gmail.com>
2024-08-02 17:52:05 +0200

    profile routes reviewed

--- models/Profile.model.js
+++ models/Profile.model.js
@@ -1,36 +1,26 @@
 const { Schema, model } = require("mongoose");
 
-
 const profileSchema = new Schema(
     {
-
         mobile: {
             type: Number,
-
         },
-
         fullName: {
             type: String,
-
         },
-
         favAirport: {
             type: String,
         },
-
         specialDiet: {
+            type: String,
             enum: ["celiac", "vegetarian", "vegan", "none", "halal"]
         },
-
         owner: {
             type: Schema.Types.ObjectId,
             ref: 'User'
         },
-
-
     },
     {
-
         timestamps: true
     }
 );
--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,26 +1,24 @@
-const router = require("express").Router()
-const Profile = require('./../models/Profile.model')
-const { isAuthenticated } = require("../middlewares/verifyToken")
-
+const express = require('express');
+const router = express.Router();
+const Profile = require('../models/Profile.model');
+const { isAuthenticated } = require("../middlewares/verifyToken");
 
 router.get("/", (req, res, next) => {
-
     Profile
         .find()
         .populate('userId')
         .then(response => res.json(response))
-        .catch(err => next(err))
-})
+        .catch(err => next(err));
+});
 
 router.get('/:profileId', (req, res, next) => {
-
-    const { profileId } = req.params
+    const { profileId } = req.params;
 
     Profile
         .findById(profileId)
         .then(oneProfile => res.json(oneProfile))
-        .catch(err => next(err))
-})
+        .catch(err => next(err));
+});
 
 router.get("/owner/:ownerId", (req, res, next) => {
     const { ownerId } = req.params;
@@ -29,37 +27,27 @@ router.get("/owner/:ownerId", (req, res, next) => {
         .find({ owner: ownerId })
         .populate('owner')
         .then(response => res.json(response))
-        .catch(err => {
-            console.error('Error fetching profiles:', err);
-            next(err);
-        });
+        .catch(err => next(err));
 });
 
-
 router.post('/', isAuthenticated, (req, res, next) => {
-
-    const { mobile, fullName, favAirport, specialDiet } = req.body
-    const { _id: owner } = req.payload
+    const { mobile, fullName, favAirport, specialDiet } = req.body;
+    const { _id: owner } = req.payload;
 
     Profile
         .create({ mobile, fullName, favAirport, specialDiet, owner })
         .then(() => res.sendStatus(201))
-        .catch(err => next(err))
-})
-
+        .catch(err => next(err));
+});
 
 router.put('/:profileId', (req, res, next) => {
-
-    const { profileId } = req.params
-    const { mobile, fullName, favAirport, specialDiet } = req.body
+    const { profileId } = req.params;
+    const { mobile, fullName, favAirport, specialDiet } = req.body;
 
     Profile
         .findByIdAndUpdate(profileId, { mobile, fullName, favAirport, specialDiet })
         .then(() => res.sendStatus(204))
-        .catch(err => next(err))
-
-})
-
-
+        .catch(err => next(err));
+});
 
 module.exports = router;
